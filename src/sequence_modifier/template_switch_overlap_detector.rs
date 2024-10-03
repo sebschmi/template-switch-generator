4a2e983370c08f9a2c84c6366e7b346ff99daa36
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2024-10-03 09:29:07 +0300

    Test template switch overlap detector.

--- src/sequence_modifier/template_switch_overlap_detector.rs
+++ src/sequence_modifier/template_switch_overlap_detector.rs
@@ -11,6 +11,7 @@ pub struct TemplateSwitchOverlapDetector {
     margin: usize,
 }
 
+#[derive(Debug, Eq, PartialEq)]
 pub enum TemplateSwitchCollision {
     Overlap,
     Independent,
@@ -18,10 +19,14 @@ pub enum TemplateSwitchCollision {
 
 impl TemplateSwitchOverlapDetector {
     pub fn new(sequence_modification_parameters: &SequenceModificationParameters) -> Self {
+        Self::from_template_switch_margin(sequence_modification_parameters.template_switch_margin)
+    }
+
+    fn from_template_switch_margin(margin: usize) -> Self {
         Self {
             template_switches: Default::default(),
             modification_stack: Default::default(),
-            margin: sequence_modification_parameters.template_switch_margin,
+            margin,
         }
     }
 
@@ -43,7 +48,7 @@ impl TemplateSwitchOverlapDetector {
                 let range_offset =
                     position.min((position as isize - length as isize + offset) as usize);
                 let range_limit = position
-                    .max((position as isize + length_difference) as usize)
+                    .max((position as isize + length as isize - length_difference) as usize)
                     .max((position as isize + offset) as usize);
                 let new_range = (range_offset - self.margin)..(range_limit + self.margin);
 
@@ -98,7 +103,7 @@ impl TemplateSwitchOverlapDetector {
                 let insertion_offset = self
                     .template_switches
                     .iter()
-                    .skip_while(|range| range.end <= new_range.start)
+                    .take_while(|range| range.end <= new_range.start)
                     .count();
                 if let Some(range) = self.template_switches.get(insertion_offset) {
                     if new_range.start < range.end && range.start < new_range.end {
@@ -125,5 +130,131 @@ impl TemplateSwitchOverlapDetector {
                 TemplateSwitchCollision::Independent
             }
         }
+    }
+}
+
+#[cfg(test)]
+#[allow(clippy::single_range_in_vec_init)]
+mod tests {
+    use crate::sequence_modifier::{
+        template_switch_overlap_detector::TemplateSwitchCollision, SequenceModification,
+    };
+
+    use super::TemplateSwitchOverlapDetector;
+
+    #[test]
+    fn simple() {
+        let mut tsod = TemplateSwitchOverlapDetector::from_template_switch_margin(10);
+        assert_eq!(
+            tsod.apply_modification(SequenceModification::TemplateSwitch {
+                position: 50,
+                length: 10,
+                offset: -5,
+                length_difference: 5,
+            }),
+            TemplateSwitchCollision::Independent
+        );
+        assert_eq!(tsod.template_switches.as_slice(), [25..65]);
+        assert_eq!(
+            tsod.apply_modification(SequenceModification::TemplateSwitch {
+                position: 100,
+                length: 10,
+                offset: -5,
+                length_difference: 5,
+            }),
+            TemplateSwitchCollision::Independent
+        );
+        assert_eq!(tsod.template_switches.as_slice(), [25..65, 70..110]);
+        assert_eq!(
+            tsod.apply_modification(SequenceModification::TemplateSwitch {
+                position: 150,
+                length: 10,
+                offset: -5,
+                length_difference: -10,
+            }),
+            TemplateSwitchCollision::Independent
+        );
+        assert_eq!(
+            tsod.template_switches.as_slice(),
+            [25..65, 70..110, 115..170]
+        );
+
+        assert_eq!(
+            tsod.apply_modification(SequenceModification::Substitution {
+                position: 60,
+                character_increment: 2
+            }),
+            TemplateSwitchCollision::Independent
+        );
+        assert_eq!(
+            tsod.apply_modification(SequenceModification::Substitution {
+                position: 70,
+                character_increment: 2
+            }),
+            TemplateSwitchCollision::Independent
+        );
+        assert_eq!(
+            tsod.apply_modification(SequenceModification::Substitution {
+                position: 80,
+                character_increment: 2
+            }),
+            TemplateSwitchCollision::Independent
+        );
+        assert_eq!(
+            tsod.apply_modification(SequenceModification::TemplateSwitch {
+                position: 200,
+                length: 10,
+                offset: 20,
+                length_difference: -1,
+            }),
+            TemplateSwitchCollision::Independent
+        );
+        assert_eq!(
+            tsod.template_switches.as_slice(),
+            [25..65, 70..110, 115..170, 190..230]
+        );
+
+        assert_eq!(
+            tsod.apply_modification(SequenceModification::Insertion {
+                position: 70,
+                source: 10,
+                length: 1,
+            }),
+            TemplateSwitchCollision::Independent
+        );
+        assert_eq!(
+            tsod.apply_modification(SequenceModification::Deletion {
+                position: 70,
+                length: 10,
+            }),
+            TemplateSwitchCollision::Independent
+        );
+        assert_eq!(
+            tsod.apply_modification(SequenceModification::TemplateSwitch {
+                position: 250,
+                length: 20,
+                offset: 10,
+                length_difference: 0,
+            }),
+            TemplateSwitchCollision::Independent
+        );
+        assert_eq!(
+            tsod.template_switches.as_slice(),
+            [25..65, 70..110, 115..170, 190..230, 240..290]
+        );
+
+        assert_eq!(
+            tsod.apply_modification(SequenceModification::TemplateSwitch {
+                position: 200,
+                length: 20,
+                offset: 10,
+                length_difference: 0,
+            }),
+            TemplateSwitchCollision::Overlap
+        );
+        assert_eq!(
+            tsod.template_switches.as_slice(),
+            [25..65, 70..110, 115..170, 190..230, 240..290]
+        );
     }
 }
